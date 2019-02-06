33d183fd050ad173c93ffbf11e29c6cf5c917584
WiFuchs <willfuchs13@gmail.com>
2019-02-06 10:36:30 -0800

    small change for travis

--- src/intMath.java
+++ src/intMath.java
@@ -1,7 +1,7 @@
 
 public class intMath {
 	public static void main(String[] args) {
-		System.out.println("5 * 3 = "+mult(5, 3));
+		System.out.println("5 * 4 = "+mult(5, 4));
 	}
 	public static int mult(int a, int b) {
 		return a*b;
